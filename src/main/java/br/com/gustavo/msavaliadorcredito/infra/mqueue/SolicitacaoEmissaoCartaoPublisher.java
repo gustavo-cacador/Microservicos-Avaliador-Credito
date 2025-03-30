33bf59724103a5472eca730e20f1a29dce22faf1
Gustavo Caçador Ronchi <gustavoirl456@outlook.com>
2025-03-30 17:30:46 -0300

    Criando o Publisher para a fila de Emissão de cartões

---
+++ src/main/java/br/com/gustavo/msavaliadorcredito/infra/mqueue/SolicitacaoEmissaoCartaoPublisher.java
@@ -0,0 +1,28 @@
+package br.com.gustavo.msavaliadorcredito.infra.mqueue;
+
+import br.com.gustavo.msavaliadorcredito.domain.model.DadosSolicitacaoEmissaoCartao;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class SolicitacaoEmissaoCartaoPublisher {
+
+    private final RabbitTemplate rabbitTemplate;
+    private final Queue queueEmissaoCartoes;
+
+    public void solicitarCartao(DadosSolicitacaoEmissaoCartao dados) throws JsonProcessingException{
+        var json = convertIntoJason(dados);
+        rabbitTemplate.convertAndSend(queueEmissaoCartoes.getName(), json);
+    }
+
+    private String convertIntoJason(DadosSolicitacaoEmissaoCartao dados) throws JsonProcessingException {
+        ObjectMapper mapper = new ObjectMapper();
+        var json = mapper.writeValueAsString(dados);
+        return json;
+    }
+}
