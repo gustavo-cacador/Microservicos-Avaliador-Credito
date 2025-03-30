9b0fe3e90fb21960b4176ce82496f88d85fe6e42
Gustavo Caçador Ronchi <gustavoirl456@outlook.com>
2025-03-30 01:26:12 -0300

    Implementando a avaliação de credito do cliente

--- src/main/java/br/com/gustavo/msavaliadorcredito/application/AvaliadorCreditoController.java
+++ src/main/java/br/com/gustavo/msavaliadorcredito/application/AvaliadorCreditoController.java
@@ -2,14 +2,12 @@ package br.com.gustavo.msavaliadorcredito.application;
 
 import br.com.gustavo.msavaliadorcredito.application.ex.DadosClienteNotFoundException;
 import br.com.gustavo.msavaliadorcredito.application.ex.ErroComunicacaoMicroservicesException;
+import br.com.gustavo.msavaliadorcredito.domain.model.DadosAvaliacao;
 import br.com.gustavo.msavaliadorcredito.domain.model.SituacaoCliente;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/avaliacoes-credito")
@@ -28,6 +26,18 @@ public class AvaliadorCreditoController {
         try {
             SituacaoCliente situacaoCliente = avaliadorCreditoService.obterSituacaoCliente(cpf);
             return ResponseEntity.ok(situacaoCliente);
+        } catch (DadosClienteNotFoundException e) {
+            return ResponseEntity.notFound().build();
+        } catch (ErroComunicacaoMicroservicesException e) {
+            return ResponseEntity.status(HttpStatus.resolve(e.getStatus())).body(e.getMessage());
+        }
+    }
+
+    @PostMapping
+    public ResponseEntity realizarAvaliacao(@RequestBody DadosAvaliacao dados) {
+        try {
+            var retornoAvaliacaoCliente = avaliadorCreditoService.realizarAvaliacao(dados.getCpf(), dados.getRenda());
+            return ResponseEntity.ok(retornoAvaliacaoCliente);
         } catch (DadosClienteNotFoundException e) {
             return ResponseEntity.notFound().build();
         } catch (ErroComunicacaoMicroservicesException e) {
--- src/main/java/br/com/gustavo/msavaliadorcredito/domain/model/Cartao.java
+++ src/main/java/br/com/gustavo/msavaliadorcredito/domain/model/Cartao.java
@@ -2,10 +2,13 @@ package br.com.gustavo.msavaliadorcredito.domain.model;
 
 import lombok.Data;
 
+import java.math.BigDecimal;
+
 @Data
-public class DadosCliente {
+public class Cartao {
 
     private Long id;
     private String nome;
-    private Integer idade;
+    private String bandeira;
+    private BigDecimal limiteBasico;
 }
