aecda1066f9fd9351da206017754b61f6cc20163
Gustavo Caçador Ronchi <gustavoirl456@outlook.com>
2025-03-30 00:18:05 -0300

    Tratando erros de comunicação entre Microservices

--- src/main/java/br/com/gustavo/msavaliadorcredito/application/AvaliadorCreditoService.java
+++ src/main/java/br/com/gustavo/msavaliadorcredito/application/AvaliadorCreditoService.java
@@ -1,11 +1,15 @@
 package br.com.gustavo.msavaliadorcredito.application;
 
+import br.com.gustavo.msavaliadorcredito.application.ex.DadosClienteNotFoundException;
+import br.com.gustavo.msavaliadorcredito.application.ex.ErroComunicacaoMicroservicesException;
 import br.com.gustavo.msavaliadorcredito.domain.model.CartaoCliente;
 import br.com.gustavo.msavaliadorcredito.domain.model.DadosCliente;
 import br.com.gustavo.msavaliadorcredito.domain.model.SituacaoCliente;
 import br.com.gustavo.msavaliadorcredito.infra.clients.CartoesResourceClient;
 import br.com.gustavo.msavaliadorcredito.infra.clients.ClienteResourceClient;
+import feign.FeignException;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
@@ -19,17 +23,21 @@ public class AvaliadorCreditoService {
 
     private final CartoesResourceClient cartoesClient;
 
-    public SituacaoCliente obterSituacaoCliente(String cpf) {
-
-        // obterDadosCliente - msclientes
-        // obter cartoes do cliente - mscartoes
-
-        ResponseEntity<DadosCliente> dadosClienteResponse = clientesClient.dadosCliente(cpf);
-        ResponseEntity<List<CartaoCliente>> cartoesResponse = cartoesClient.getCartoesByCliente(cpf);
-        return SituacaoCliente
-                .builder()
-                .cliente(dadosClienteResponse.getBody())
-                .cartoes(cartoesResponse.getBody())
-                .build();
+    public SituacaoCliente obterSituacaoCliente(String cpf) throws DadosClienteNotFoundException, ErroComunicacaoMicroservicesException {
+        try {
+            ResponseEntity<DadosCliente> dadosClienteResponse = clientesClient.dadosCliente(cpf); // obterDadosCliente - msclientes
+            ResponseEntity<List<CartaoCliente>> cartoesResponse = cartoesClient.getCartoesByCliente(cpf); // obter cartoes do cliente - mscartoes
+            return SituacaoCliente
+                    .builder()
+                    .cliente(dadosClienteResponse.getBody())
+                    .cartoes(cartoesResponse.getBody())
+                    .build();
+        } catch (FeignException.FeignClientException e) {
+            int status = e.status();
+            if (HttpStatus.NOT_FOUND.value() == status) {
+                throw new DadosClienteNotFoundException();
+            }
+            throw new ErroComunicacaoMicroservicesException(e.getMessage(), status);
+        }
     }
 }
