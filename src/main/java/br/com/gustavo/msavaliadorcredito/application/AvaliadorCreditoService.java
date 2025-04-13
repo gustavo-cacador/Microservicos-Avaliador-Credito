b8a85219eacc836d82770d86dfe591d09b7b3c4d
Gustavo Caçador Ronchi <gustavoirl456@outlook.com>
2025-04-13 03:50:50 -0300

    Implementando a avaliação de credito do cliente

--- src/main/java/br/com/gustavo/msavaliadorcredito/application/AvaliadorCreditoService.java
+++ src/main/java/br/com/gustavo/msavaliadorcredito/application/AvaliadorCreditoService.java
@@ -2,9 +2,7 @@ package br.com.gustavo.msavaliadorcredito.application;
 
 import br.com.gustavo.msavaliadorcredito.application.ex.DadosClienteNotFoundException;
 import br.com.gustavo.msavaliadorcredito.application.ex.ErroComunicacaoMicroservicesException;
-import br.com.gustavo.msavaliadorcredito.domain.model.CartaoCliente;
-import br.com.gustavo.msavaliadorcredito.domain.model.DadosCliente;
-import br.com.gustavo.msavaliadorcredito.domain.model.SituacaoCliente;
+import br.com.gustavo.msavaliadorcredito.domain.model.*;
 import br.com.gustavo.msavaliadorcredito.infra.clients.CartoesResourceClient;
 import br.com.gustavo.msavaliadorcredito.infra.clients.ClienteResourceClient;
 import feign.FeignException;
@@ -13,7 +11,9 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import java.math.BigDecimal;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -32,6 +32,40 @@ public class AvaliadorCreditoService {
                     .cliente(dadosClienteResponse.getBody())
                     .cartoes(cartoesResponse.getBody())
                     .build();
+        } catch (FeignException.FeignClientException e) {
+            int status = e.status();
+            if (HttpStatus.NOT_FOUND.value() == status) {
+                throw new DadosClienteNotFoundException();
+            }
+            throw new ErroComunicacaoMicroservicesException(e.getMessage(), status);
+        }
+    }
+
+    public RetornoAvaliacaoCliente realizarAvaliacao(String cpf, Long renda) throws DadosClienteNotFoundException, ErroComunicacaoMicroservicesException {
+        try {
+            ResponseEntity<DadosCliente> dadosClienteResponse = clientesClient.dadosCliente(cpf); // obterDadosCliente - msclientes
+            ResponseEntity<List<Cartao>> cartoesResponse = cartoesClient.getCartoesRendaAte(renda); // obter cartao renda ate - mscartoes
+
+            List<Cartao> cartoes = cartoesResponse.getBody();
+            var listaCartoesAprovados = cartoes.stream().map(cartao -> {
+
+                DadosCliente dadosCliente = dadosClienteResponse.getBody();
+
+                BigDecimal limiteBasico = cartao.getLimiteBasico();
+                BigDecimal idadeBD = BigDecimal.valueOf(dadosCliente.getIdade());
+                var fator = idadeBD.divide(BigDecimal.valueOf(10));
+                BigDecimal limiteAprovado = fator.multiply(limiteBasico);
+
+                CartaoAprovado aprovado = new CartaoAprovado();
+                aprovado.setCartao(cartao.getNome());
+                aprovado.setBandeira(cartao.getBandeira());
+                aprovado.setLimiteAprovado(limiteAprovado);
+
+                return aprovado;
+            }).collect(Collectors.toList());
+
+            return new RetornoAvaliacaoCliente(listaCartoesAprovados);
+
         } catch (FeignException.FeignClientException e) {
             int status = e.status();
             if (HttpStatus.NOT_FOUND.value() == status) {
